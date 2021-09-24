2a64b22885a8774a627bccce477f22931c106d9c
senguyen1011 <seantanhnguyen@gmail.com>
2021-09-24 12:33:02 -0400

    Add remove member dialog

--- src/client/pwcCollabTab/PwcCollabTab.tsx
+++ src/client/pwcCollabTab/PwcCollabTab.tsx
@@ -14,115 +14,115 @@ import { GuestList } from "./components/GuestList";
  * The Main Tab View
  */
 export const PwcCollabTab = () => {
-  const [{ inTeams, theme, context }] = useTeams();
-  const [entityId, setEntityId] = useState<string | undefined>();
-  const [teamId, setTeamId] = useState<string | undefined>();
-  const [name, setName] = useState<string>();
-  const [error, setError] = useState<string>();
-  const [selectedMenuItem, setSelectedMenuItem] = useState('add');
+    const [{ inTeams, theme, context }] = useTeams();
+    const [entityId, setEntityId] = useState<string | undefined>();
+    const [teamId, setTeamId] = useState<string | undefined>();
+    const [name, setName] = useState<string>();
+    const [error, setError] = useState<string>();
+    const [selectedMenuItem, setSelectedMenuItem] = useState('add');
 
-  const [ssoToken, setSsoToken] = useState<string>();
-  const [msGraphOboToken, setMsGraphOboToken] = useState<string>();
+    const [ssoToken, setSsoToken] = useState<string>();
+    const [msGraphOboToken, setMsGraphOboToken] = useState<string>();
 
-  /**
-   * Initially checks to see if the app is running in an instance of Teams
-   */
-  useEffect(() => {
-    if (inTeams === true) {
-      microsoftTeams.authentication.getAuthToken({
-        // If the app is able to successfully retrieve an AuthToken, it will 
-        // save it to state
-        successCallback: (token: string) => {
-          const decoded: { [key: string]: any; } = jwtDecode(token) as { [key: string]: any; };
-          setName(decoded!.name);
-          setSsoToken(token);
-          microsoftTeams.appInitialization.notifySuccess();
-        },
-        failureCallback: (message: string) => {
-          setError(message);
-          microsoftTeams.appInitialization.notifyFailure({
-            reason: microsoftTeams.appInitialization.FailedReason.AuthFailed,
-            message
-          });
-        },
-        resources: [process.env.TAB_APP_URI as string]
-      });
-    } else {
-      setEntityId("Not in Microsoft Teams");
+    /**
+     * Initially checks to see if the app is running in an instance of Teams
+     */
+    useEffect(() => {
+        if (inTeams === true) {
+        microsoftTeams.authentication.getAuthToken({
+            // If the app is able to successfully retrieve an AuthToken, it will 
+            // save it to state
+            successCallback: (token: string) => {
+            const decoded: { [key: string]: any; } = jwtDecode(token) as { [key: string]: any; };
+            setName(decoded!.name);
+            setSsoToken(token);
+            microsoftTeams.appInitialization.notifySuccess();
+            },
+            failureCallback: (message: string) => {
+            setError(message);
+            microsoftTeams.appInitialization.notifyFailure({
+                reason: microsoftTeams.appInitialization.FailedReason.AuthFailed,
+                message
+            });
+            },
+            resources: [process.env.TAB_APP_URI as string]
+        });
+        } else {
+        setEntityId("Not in Microsoft Teams");
+        }
+    }, [inTeams]);
+
+    /**
+    * Get context of the Teams tab
+    */
+    useEffect(() => {
+        if (context) {
+            setEntityId(context.entityId);
+            setTeamId(context.groupId);
+        }
+    }, [context]);
+
+    /**
+     * Sets the MsGraphOboToken for the Graph API
+     */
+    const exchangeSsoTokenForOboToken = useCallback(async () => {
+        const response = await fetch(`/exchangeSsoTokenForOboToken/?ssoToken=${ssoToken}`);
+        const responsePayload = await response.json();
+        if (response.ok) {
+        setMsGraphOboToken(responsePayload.access_token);
+        } else {
+        if (responsePayload!.error === "consent_required") {
+            setError("consent_required");
+        } else {
+            setError("unknown SSO error");
+        }
+        }
+    }, [ssoToken]);
+
+    useEffect(() => {
+    // if the SSO token is defined...
+        if (ssoToken && ssoToken.length > 0) {
+        exchangeSsoTokenForOboToken();
+        }
+    }, [exchangeSsoTokenForOboToken, ssoToken]);
+
+    /**
+     * Handles the menu
+     */
+    const handleMenuSelect = (selected) => {
+        setSelectedMenuItem(selected);
     }
-  }, [inTeams]);
 
-  /**
-   * Get context of the Teams tab
-   */
-  useEffect(() => {
-    if (context) {
-      setEntityId(context.entityId);
-      setTeamId(context.groupId);
-    }
-  }, [context]);
+    /**
+     * The render() method to create the UI of the tab
+     */
+    return (
+        <Provider theme={theme}>
+        <Flex column gap="gap.smaller" padding="padding.medium">
+            <Header content="External Guest Collaboration" />
+            {error && <div><Text content={`An SSO error occurred ${error}`} /></div>}
+        </Flex>
+        <Divider />
 
-  /**
-   * Sets the MsGraphOboToken for the Graph API
-   */
-  const exchangeSsoTokenForOboToken = useCallback(async () => {
-    const response = await fetch(`/exchangeSsoTokenForOboToken/?ssoToken=${ssoToken}`);
-    const responsePayload = await response.json();
-    if (response.ok) {
-      setMsGraphOboToken(responsePayload.access_token);
-    } else {
-      if (responsePayload!.error === "consent_required") {
-        setError("consent_required");
-      } else {
-        setError("unknown SSO error");
-      }
-    }
-  }, [ssoToken]);
+        <Flex gap="gap.large" padding="padding.medium"
+            styles={{
+            padding: '1rem 4rem 4rem 1rem'
+            }}>
+            <NavMenu selected={selectedMenuItem} callback={handleMenuSelect} />
+            {selectedMenuItem === "add" && (
+            <GuestForm token={msGraphOboToken} teamId={teamId}/>
+            )}
 
-  useEffect(() => {
-  // if the SSO token is defined...
-    if (ssoToken && ssoToken.length > 0) {
-      exchangeSsoTokenForOboToken();
-    }
-  }, [exchangeSsoTokenForOboToken, ssoToken]);
+            {selectedMenuItem === "status" && (
+            <GuestList token={msGraphOboToken} teamId={teamId} />
+            )}
 
-  /**
-   * Handles the menu
-   */
-  const handleMenuSelect = (selected) => {
-    setSelectedMenuItem(selected);
-  }
-
-  /**
-   * The render() method to create the UI of the tab
-   */
-  return (
-    <Provider theme={theme}>
-      <Flex column gap="gap.smaller" padding="padding.medium">
-        <Header content="External Guest Collaboration" />
-        {error && <div><Text content={`An SSO error occurred ${error}`} /></div>}
-      </Flex>
-      <Divider />
-
-      <Flex gap="gap.large" padding="padding.medium"
-        styles={{
-          padding: '1rem 4rem 4rem 1rem'
-        }}>
-        <NavMenu selected={selectedMenuItem} callback={handleMenuSelect} />
-        {selectedMenuItem === "add" && (
-          <GuestForm token={msGraphOboToken} teamId={teamId}/>
-        )}
-
-        {selectedMenuItem === "status" && (
-          <GuestList token={msGraphOboToken} teamId={teamId} />
-        )}
-
-        {selectedMenuItem === "faq" && (
-          <Fragment>Add Documentation here</Fragment>
-        )}
-      </Flex>
-    </Provider>
-  );
+            {selectedMenuItem === "faq" && (
+            <Fragment>Add Documentation here</Fragment>
+            )}
+        </Flex>
+        </Provider>
+    );
 };
 
 
--- src/client/pwcCollabTab/components/NavMenu.tsx
+++ src/client/pwcCollabTab/components/NavMenu.tsx
@@ -1,29 +1,29 @@
-import * as React from 'react';
+import * as React from "react";
 import { useState } from "react";
 import { Menu } from "@fluentui/react-northstar";
 
 export function NavMenu(props) {
-  const [selected, setSelected] = useState(props.selected);
-  
-  const steps = ['add', 'status', 'faq'];
-  const stepNames: { [key: string]: string } = {
-    add: 'Invite Guest',
-    status: 'View Guests',
-    faq: 'FAQ'
-  };
+    const [selected, setSelected] = useState(props.selected);
 
-  const menuItems = steps.map(step => {
-    return {
-      key: step,
-      content: stepNames[step] || '',
-      onClick: () => {
-        setSelected(step);
-        props.callback(step);
-      }
+    const steps = ["add", "status", "faq"];
+    const stepNames: { [key: string]: string } = {
+        add: "Invite Guest",
+        status: "View Guests",
+        faq: "FAQ"
     };
-  });
 
-  return (
-    <Menu defaultActiveIndex={0} items={menuItems} vertical pointing />
-  );
+    const menuItems = steps.map(step => {
+        return {
+            key: step,
+            content: stepNames[step] || "",
+            onClick: () => {
+                setSelected(step);
+                props.callback(step);
+            }
+        };
+    });
+
+    return (
+        <Menu defaultActiveIndex={0} items={menuItems} vertical pointing />
+    );
 }
