4c6d09b014b8881f5e4c9cb4a1dda26461516948
Xinyue Shu <shuxinyu@msu.edu>
2021-09-29 13:56:58 +0800

    update GetManager function

--- src/data/GetManager.tsx
+++ src/data/GetManager.tsx
@@ -2,26 +2,34 @@ import * as React from "react";
 
 
 export function GetManager(){
-    const name="manager";
+    const name="test_manager";
     const email="testmanager@email";
-    const id="";
     var raw_data = require('./information.json');
     var  json= JSON.stringify(raw_data);
     var data = JSON.parse(json);
-    var output=data["2"]["email"];
-    var allEmails =Array();
+    //var output=data["2"]["email"];  test
+    var ret =new Map();
     for (var i in data){
-        var info = data[i]["email"];
-        console.log(info);
-        allEmails.push(info);
+        var allinfo =Array();
+        var info_email = data[i]["email"];
+        var info_name =data[i]["firstname"]+","+data[i]["lastname"];
+        var domain = data[i]["domain"];
+        allinfo.push(info_name);
+        allinfo.push(info_email);
+        console.log(allinfo);   //test
+        ret.set(domain,allinfo);
     }
     
+    //testing
     return (
         <div className="GetManager">
             <p>{email}</p>
             <p>{name}</p>
-            <p>{allEmails}</p>
+            <p>{ret}</p>
         </div>
       );
     
+     //return ret;   --->return the map
+    
 }
+
