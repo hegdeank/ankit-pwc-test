9a155ed50e9ffe94ad3e3292fc59ab813b208c03
Xinyue Shu <shuxinyu@msu.edu>
2021-10-20 21:51:32 +0800

    Add some gaphapi for creating access package

--- src/client/pwcCollabTab/services/GraphService.tsx
+++ src/client/pwcCollabTab/services/GraphService.tsx
@@ -1,4 +1,5 @@
 const baseUrl = "https://graph.microsoft.com/v1.0/";
+const betaUrl = "https://graph.microsoft.com/beta/";
 
 export async function getUsers(token, select) {
     const endpoint = `${baseUrl}users?$select=${select}`;
@@ -91,7 +92,7 @@ export async function addTeamMember(token, teamId, userId): Promise<any> {
 }
 
 export async function getTeamMembers(token, teamId): Promise<any> {
-    const endpoint = `${baseUrl}teams/${teamId}/members`;
+    const endpoint = `${betaUrl}teams/${teamId}/members`;
 
     const requestObject = {
         method: "GET",
@@ -161,3 +162,98 @@ export async function getCurrentUser(token) {
     }
     return;
 } 
+
+//create a catalog and return the id of the catalog
+export async function getCatalogID(token){
+    const endpoint = `${baseUrl}identityGovernance/entitlementManagement/accessPackageCatalogs?$filter=(displayName eq 'General')`;
+    const requestObject = {
+        method: "GET",
+        headers: {
+            authorization: `bearer ${token}`,
+            "content-type": "application/json"
+        }
+    };
+
+    // Fetch response
+    const response = await fetch(endpoint, requestObject);
+    if (response.ok) {
+        var raw_data=await response.json();
+        var  json= JSON.stringify(raw_data);
+        var data = JSON.parse(json);
+        var value=data.value[0];
+        var id = value.id;
+        return id;
+    }
+    return;
+}
+
+// Need to find the group id you created
+// First find all your joined groups
+// Then find the team ID where displayteamName = input_team_name
+//return group id and description
+export async function getGroupID(token, team_name): Promise<any> {
+    const endpoint = `${baseUrl}me/transitiveMemberOf/microsoft.graph.group?$count=true`;
+    const requestObject = {
+        method: "POST",
+        headers: {
+            authorization: `bearer ${token}`,
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(team_name)   //????
+    };
+
+    // Fetch response
+    const response = await fetch(endpoint, requestObject);
+    if (response.ok) {
+        var raw_data=await response.json();
+        var  json= JSON.stringify(raw_data);
+        var data = JSON.parse(json);
+        var value=data.value;
+        for (var idx in value){
+            var group = value[idx];
+            var team = group.displayName;
+            var id = group.id;
+            var description = group.description;
+            console.log(team);
+            console.log(id);
+            if (team == team_name){  // find the id
+                return {id,description};
+            }
+        }
+    }
+}
+
+// export async function addGroupToCatalog(token, team_name,catalogID,groupID,description): Promise<any> {
+//     const endpoint = `${baseUrl}me/transitiveMemberOf/microsoft.graph.group?$count=true`;
+//     const requestObject = {
+//         method: "POST",
+//         headers: {
+//             authorization: `bearer ${token}`,
+//             "content-type": "application/json"
+            
+//         },
+//         body: {
+//             "catalogId": `${catalogID}`,
+//             "requestType": "AdminAdd",
+//             "justification": "",
+//             "accessPackageResource": {
+//               "displayName": `${description}`,
+//               "description": `${team_name}`,
+//               "resourceType": "AadGroup",
+//               "originId": `${groupID}`,
+//               "originSystem": "AadGroup"
+//             }
+//           }   
+//         };
+
+//     // Fetch response
+//     const response = await fetch(endpoint, requestObject);
+//     if (response.ok) {
+//         var raw_data=await response.json();
+//         var  json= JSON.stringify(raw_data);
+//         var data = JSON.parse(json);
+//         var value=data.value;
+//     }
+//     return;
+
+// }
