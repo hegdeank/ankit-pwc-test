81b99054db596532c93824aabc96a2baf7d07716
senguyen1011 <seantanhnguyen@gmail.com>
2021-10-26 11:26:50 -0400

    Add Skeleton Loader

--- src/client/pwcCollabTab/components/MembersView.tsx
+++ src/client/pwcCollabTab/components/MembersView.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import { Fragment, useEffect, useState, useCallback } from "react";
 import { useTeams } from "msteams-react-base-component";
 import {
-    Accordion, Input, Flex, Loader, Text, Grid
+    Accordion, Card, Input, Flex, Grid, Loader, Skeleton, Text
 } from "@fluentui/react-northstar";
 import { SearchIcon } from "@fluentui/react-icons-northstar";
 import {
-    getUser, getTeamMembers,
-    deleteUser, removeTeamMember,getCurrentUser, getUserPresence, getUserPhoto
+    getUser, getTeamMembers, deleteUser, removeTeamMember,
+    getCurrentUser, getUserPresence, getUserPhoto
 } from "../services/GraphService";
-import { getUserByEmail, getApproverByEmail } from "../services/PwCService";
+import { getUserByEmail } from "../services/PwCService";
 import { InviteDialog } from "./InviteDialog";
 import { MemberCard } from "./uiComponents/MemberCard";
 
@@ -40,15 +40,23 @@ const CardHeader = () => {
 }
 
 export function MembersView(props) {
-    const [{ theme, context }] = useTeams();
-    const [ownerRows, setOwnerRows] = useState<any[]>([]);
-    const [memberRows, setMemberRows] = useState<any[]>([]);
-    const [pendingRows, setPendingRows] = useState<any[]>([]);
+    const [ownerRows, setOwnerRows] = useState<any[] | undefined>();
+    const [memberRows, setMemberRows] = useState<any[] | undefined>();
+    const [pendingRows, setPendingRows] = useState<any[] | undefined>();
     const [teamMembers, setTeamMembers] = useState<any[]>([]);
     const [activePanels, setActivePanels] = useState<number[]>([]);
     const [memberType, setMemberStatus] = useState<number>();
     const token = props.token;
     const teamId = props.teamId;
+
+    const isLoading = () => {
+        if ((!ownerRows || ownerRows.length === 0) && 
+            (!memberRows || memberRows.length === 0) && 
+            (!pendingRows || pendingRows.length === 0)) {
+            return true;
+        } 
+        return false;
+    }
 
     const getTeamGuests = useCallback(async () => {
         if (!token) { return; }
@@ -66,7 +74,7 @@ export function MembersView(props) {
         
         const userKey = await getUserByEmail(userEmail);
         const userParams = userKey.data.length; // data from query that holds: id, firstname, lastname, email, permission
-        console.log("User Status, If 1 then internal, if 0 then external: "+ userParams);
+
         setMemberStatus(userParams);
         setTeamMembers(teamMemberResponse);
     }, [token]);
@@ -152,13 +160,13 @@ export function MembersView(props) {
 
     useEffect(() => {
         let panels: number[] = [];
-        if (ownerRows.length !== 0) {
+        if (ownerRows && ownerRows.length !== 0) {
             panels.push(0);
         } 
-        if (memberRows.length !== 0) {
+        if (memberRows && memberRows.length !== 0) {
             panels.push(1);
         }
-        if (pendingRows.length !== 0) {
+        if (pendingRows && pendingRows.length !== 0) {
             panels.push(2);
         }
         setActivePanels(panels);
@@ -176,75 +184,111 @@ export function MembersView(props) {
         }
     }
 
+    const MemberAccordion = () => {
+        return (
+            <Accordion activeIndex={activePanels} panels={
+                [
+                    {
+                        title: {
+                            content: (
+                                <span>
+                                    <Text weight="bold" disabled={ownerRows?.length === 0 ? true : false} content="Owners " />
+                                    <Text disabled={ownerRows?.length === 0 ? true : false} content={`(${ownerRows?.length})`} />
+                                </span>
+                            ),
+                            disabled: ownerRows?.length === 0 ? true : false,
+                            onClick: () => handlePanelClick(0)
+                        },
+                        content:(
+                            <Flex column gap="gap.smaller">
+                                <CardHeader />
+                                {ownerRows}
+                            </Flex>
+                        )
+                    },
+                    {
+                        title: {
+                            content: (
+                                <span>
+                                    <Text weight="bold" disabled={memberRows?.length === 0 ? true : false} content="Members and Guests " />
+                                    <Text disabled={memberRows?.length === 0 ? true : false} content={`(${memberRows?.length})`} />
+                                </span>
+                            ),
+                            disabled: memberRows?.length === 0 ? true : false,
+                            onClick: () => handlePanelClick(1)
+                        },
+                        content: (
+                            <Flex column gap="gap.smaller">
+                                <CardHeader />
+                                {memberRows}
+                            </Flex>
+                        )
+                    },
+                    {
+                        title: {
+                            content: (
+                                <span>
+                                    <Text weight="bold" disabled={pendingRows?.length === 0 ? true : false} content="Pending Acceptance " />
+                                    <Text disabled={pendingRows?.length === 0 ? true : false} content={`(${pendingRows?.length})`} />
+                                </span>
+                            ),
+                            disabled: pendingRows?.length === 0 ? true : false,
+                            onClick: () => handlePanelClick(2)
+                        },
+                        content: (
+                            <Flex column gap="gap.smaller">
+                                <CardHeader />
+                                {pendingRows}
+                            </Flex>
+                        )
+                    }
+                ]
+            } />
+        );
+    }
+
+    const MemberLoading = () => {
+        const CardLoading = () => {
+            return (
+                <Card fluid>
+                    <Flex vAlign="center" gap="gap.large">
+                        <Skeleton.Avatar size="large" />
+                        <Skeleton.Line width="80%" />
+                    </Flex>
+                </Card>
+            );
+        }
+
+        return (
+            <Skeleton animation="pulse">
+                <Flex column gap="gap.medium">
+                    <Skeleton.Line width="6rem" />
+                    <CardLoading />
+                    <Skeleton.Line width="6rem" />
+                    <CardLoading />
+                    <CardLoading />
+                    <Skeleton.Line width="6rem" />
+                    <CardLoading />
+                    <CardLoading />
+                </Flex>
+            </Skeleton>
+        );
+    }
+
     return (
         <Fragment>
             
             <Flex column fill={true} gap="gap.large">
                 <Flex space="between">
                     <Input icon={<SearchIcon />} placeholder="Search for members" />
-                    {memberType === 1 && (
+                    { memberType === 1 && 
                         <InviteDialog token={token} teamId={teamId} teamName={props.teamName}/>
-                        )}
+                    }
                 </Flex>
-                
-                <Accordion activeIndex={activePanels} panels={
-                    [
-                        {
-                            title: {
-                                content: (
-                                    <span>
-                                        <Text weight="bold" disabled={ownerRows.length === 0 ? true : false} content="Owners " />
-                                        <Text disabled={ownerRows.length === 0 ? true : false} content={`(${ownerRows.length})`} />
-                                    </span>
-                                ),
-                                disabled: ownerRows.length === 0 ? true : false,
-                                onClick: () => handlePanelClick(0)
-                            },
-                            content:(
-                                <Flex column gap="gap.smaller">
-                                    <CardHeader />
-                                    {ownerRows}
-                                </Flex>
-                            )
-                        },
-                        {
-                            title: {
-                                content: (
-                                    <span>
-                                        <Text weight="bold" disabled={memberRows.length === 0 ? true : false} content="Members and Guests " />
-                                        <Text disabled={memberRows.length === 0 ? true : false} content={`(${memberRows.length})`} />
-                                    </span>
-                                ),
-                                disabled: memberRows.length === 0 ? true : false,
-                                onClick: () => handlePanelClick(1)
-                            },
-                            content: (
-                                <Flex column gap="gap.smaller">
-                                    <CardHeader />
-                                    {memberRows}
-                                </Flex>
-                            )
-                        },
-                        {
-                            title: {
-                                content: (
-                                    <span>
-                                        <Text weight="bold" disabled={pendingRows.length === 0 ? true : false} content="Pending Acceptance " />
-                                        <Text disabled={pendingRows.length === 0 ? true : false} content={`(${pendingRows.length})`} />
-                                    </span>
-                                ),
-                                disabled: pendingRows.length === 0 ? true : false,
-                                onClick: () => handlePanelClick(2)
-                            },
-                            content: (
-                                <Flex column gap="gap.smaller">
-                                    <CardHeader />
-                                    {pendingRows}
-                                </Flex>
-                            )
-                        }
-                    ]
-                } />
+
+                {/* { isLoading() && <Loader label="Loading members..." /> } */}
+                { isLoading() && <MemberLoading /> }
+                { !isLoading() && <MemberAccordion /> }
             </Flex>
         </Fragment>
     );
